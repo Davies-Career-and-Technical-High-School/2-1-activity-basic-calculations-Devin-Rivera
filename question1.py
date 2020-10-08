b88b45ab09900a3eee34de145e476fdc8aad219f
Devin Woloszyn-Rivera <devin.woloszyn-rivera@daviestech.org>
2020-10-08 19:24:41 +0000

    Added work

---
+++ question1.py
@@ -0,0 +1,4 @@
+num = int(input("Enter an interger: "))
+print(num + int("1"))
+print(num + int("2"))
+print(num + int("3"))
---
+++ question2.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter a number here "))
+num2 = float(input("Enter a number here "))
+num3 = float(input("Enter a number here "))
+
+print( num1 + num2 + num3 )
